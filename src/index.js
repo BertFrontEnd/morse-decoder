a6e4a5d3ac921afa50485fa6952c443be8e6e47e
Sergey <48886417+BertFrontEnd@users.noreply.github.com>
2021-08-06 13:35:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = expr.match(/.{10}/g);
+
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i].replace(/10/g, '.');
+    array[i] = array[i].replace(/11/g, '-');
+    array[i] = array[i].replace(/0/g, '');
+  }
+
+  let string = '';
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === '**********') {
+      string += ' ';
+    }
+
+    for (element in MORSE_TABLE) {
+      if (array[i] === element) {
+        string += MORSE_TABLE[element];
+      }
+    }
+  }
+
+  return string;
 }
 
 module.exports = {
